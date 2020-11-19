b0b0285bcfb079ef88aa66da133f2e89d57d99ce
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-11-18 23:51:21 -0800

    removing bugged code

--- src/pages/demographic/multiSelect.js
+++ src/pages/demographic/multiSelect.js
@@ -13,19 +13,9 @@ export default class MultiSelect extends React.Component {
     return (
       <div className={style.demographicContainer}>
         <div className={style.demographicQuestions}>
-        <h2>{store.multiSelect[0].questionText}</h2>
-        <form onSubmit={this.props.nextFunction()}>
-          {store.multiSelect[0].answerOptions.map((answerOptions) => (
-              <div className={style.checkboxContainer}>
-                <input type="checkbox" onChange={() => console.log(answerOptions.answerText)}></input>
-                <span className={style.checked}></span>
-                {answerOptions.answerText}
               </div>
           ))}
-          <button>Looks Good 🡒</button>
-        </form>
         </div>
-        <div className={style.buttonContainer}><button onClick={this.props.backFunction}>🡐</button></div>
       </div>
     );
   }
