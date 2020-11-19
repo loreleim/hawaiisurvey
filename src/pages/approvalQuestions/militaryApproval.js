c747e1aa15c8da6175e13e4ccd48f235ab0d4a81
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-11-18 23:46:41 -0800

    military approval template

---
+++ src/pages/approvalQuestions/militaryApproval.js
@@ -0,0 +1,24 @@
+import React from "react";
+import style from "./index.module.scss";
+import store from "../../database/database.js";
+
+export default class MilitaryApproval extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+        <div className={style.approval2Container}>
+        <h2 className={style.approvalLast}>{store.approvalQuestions[this.props.approvalStep - 8]}?</h2>
+        <div className={style.template2}>
+          {store.options.map((options, index) => (
+            <div key={options} className={style.optionsContainer}>
+            <button onClick={() => this.props.nextPage(options)}>{options}</button>
+            </div>
+          ))}
+        </div>
+        </div>
+      </div>
+    );
+  }
+}
