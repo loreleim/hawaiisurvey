cd13c3f55737485dfa200dfa471bea5f1471c0a8
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-11-18 23:45:36 -0800

    approval Question template

--- src/pages/approvalQuestions/index.js
+++ src/pages/approvalQuestions/index.js
@@ -7,15 +7,18 @@ export default class Approval extends React.Component {
   render() {
 
     return (
-      <div className={style.approvalContainer}>
-        <h1>Do you</h1>
-        {store.options.map((options, index) => (
-          <div key={options} className={style.optionsContainer}>
-            <button onClick={() => this.props.nextPage(options)}>{options}</button>
-            <h3>{index === 1 ? "" : "or"}</h3>
+      <div>
+        <div className={style.approvalContainer}>
+          <h1>Do you</h1>
+          <div className={style.approveOrDisapprove}>
+            {store.options.map((options, index) => (
+              <div key={options} className={style.optionsContainer}>
+                <button onClick={() => this.props.nextPage(options)}>{options}</button>
+              </div>
+            ))}
           </div>
-        ))}
-        <h2 className={style.approvalLast}>of {store.approvalQuestions[this.props.approvalStep - store.demographicQuestions.length - store.context.length - 1]}?</h2>
+          <h2 className={style.approvalLast}>of {store.approvalQuestions[this.props.approvalStep - 6]}?</h2>
+        </div>
       </div>
     );
   }
