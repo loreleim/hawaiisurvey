3f4f8866bbc13879f2d10f298ab9d474ad1cf5ab
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-11-18 23:46:10 -0800

    approval Question edits

---
+++ src/pages/approvalQuestions/marineApproval.js
@@ -0,0 +1,25 @@
+import React from "react";
+import style from "./index.module.scss";
+import store from "../../database/database.js";
+
+export default class MarineApproval extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+        <div className={style.approvalContainer}>
+          <h1>Do you</h1>
+          <div className={style.approveOrDisapprove}>
+            {store.options.map((options, index) => (
+              <div key={options} className={style.optionsContainer}>
+                <button onClick={() => this.props.nextPage(options)}>{options}</button>
+              </div>
+            ))}
+          </div>
+          <h2 className={style.approvalLast}>of {store.approvalQuestions[this.props.approvalStep - 7]}?</h2>
+        </div>
+      </div>
+    );
+  }
+}
