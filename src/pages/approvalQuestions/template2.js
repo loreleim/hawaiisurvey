6524450e13a3434375b9979579e2dbd7272b9728
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-11-18 23:46:55 -0800

    template w/o "do you?"

--- src/pages/approvalQuestions/template2.js
+++ src/pages/approvalQuestions/template2.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import style from "./index.module.scss";
+import store from "../../database/database.js";
 
-class Template2 extends React.Component {
+class ApprovalSecond extends React.Component {
     render() {
         return (
-            <div className={style.approvalContainer}>
-                <h2 className={style.approvalLast}>of {store.approvalQuestions[this.props.approvalStep - store.demographicQuestions.length - store.context.length - 1]}?</h2>
-                {store.options.map((options, index) => (
-                <div key={options} className={style.optionsContainer}>
-                    <button onClick={() => this.props.nextPage(options)}>{options}</button>
-                    <h3>{index === 1 ? "" : "or"}</h3>
+            <div>
+                <div className={style.approval2Container}>
+                <h2 className={style.approvalLast}>{store.approvalQuestions[this.props.approvalStep-6]}?</h2>
+                <div className={style.template2}>
+                    {store.options.map((options, index) => (
+                    <div key={options} className={style.optionsContainer}>
+                        <button onClick={() => this.props.nextPage(options)}>{options}</button>
+                        {index === 0 ? <h3>or</h3> : null}
+                    </div>
+                    ))}
                 </div>
-                ))}
-          </div>
+                </div>
+            </div>
         );
     }
 }
 
-export default Template2;
+export default ApprovalSecond;
