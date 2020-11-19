0c79310cca087c224a5725218bac4191f9c373bf
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-11-19 00:18:31 -0800

    Working end page & confetti! woohoo

--- src/pages/thankYou/index.js
+++ src/pages/thankYou/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import style from "./index.module.scss";
-
+import Confetti from 'react-confetti';
 export default class ThankYou extends React.Component {
 
   render() {
 
     return (
-      <div className={style.thankContainer}>
+      <div>
+        <div className={style.confettiContainer}>
+          <Confetti width={window.innerWidth} height={window.innerHeight} numberOfPieces={100}/>
+        </div>
+        <div className={style.thankContainer}>
           <h1>Mahalo</h1>
           <h3>Your responses have been submitted successfully!</h3>
+        </div>
       </div>
     );
   }
