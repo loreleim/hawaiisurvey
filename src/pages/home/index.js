9a4d1f35d41d8c7199bdbbc3e033edc66d5246d7
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2020-11-27 22:35:41 -0800

    apostrophe fix for language

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -11,7 +11,7 @@ export default class Home extends React.Component {
         <div className={style.homeText}>
           <h1>Hawai'i Industry Survey</h1>
           <h2>locals on Hawai'i industries</h2>
-          <p>Isolated pacific islands such as Hawai’i currently face economic toll due to the pandemic’s effect on industries such as tourism. When existing in an economy without it’s main stimulant, how are perceptions of “successful” industries changing amongst local communities?</p>
+          <p>Isolated pacific islands such as Hawai’i currently face economic toll due to the pandemic’s effect on industries such as tourism. When existing in an economy without its main stimulant, how are perceptions of “successful” industries changing amongst local communities?</p>
         </div>
         <button onClick={this.props.function} className={style.nextButton}>start</button>
       </div>
